e1e3c3ac596e716cff078f79423dc27d456492be
dimonProto <dimonvkedax@gmail.com>
2020-12-02 18:10:14 +0200

    Timer

---
+++ common.js
@@ -0,0 +1,32 @@
+
+let newYear = "1 Jan 2021";
+
+let dayElement = document.getElementById('day');
+let hourElement = document.getElementById('hour');
+let minutesElement = document.getElementById('minutes');
+let secondsElement = document.getElementById('seconds');
+
+function timerforNewYear(){
+    let newYearDate = new Date(newYear);
+    let dateToDay = new Date();
+
+    let totalSeconds = (newYearDate - dateToDay) / 1000;
+   
+    let days = Math.floor((totalSeconds / 3600) / 24);
+    let hours = Math.floor(totalSeconds / 3600) % 24;
+    let minutes = Math.floor(totalSeconds / 60) % 60;
+    let seconds = Math.floor(totalSeconds % 60);
+
+    dayElement.innerHTML = days;
+    hourElement.innerHTML = zero(hours);
+    minutesElement.innerHTML = zero(minutes);
+    secondsElement.innerHTML = zero(seconds);
+
+}
+timerforNewYear()
+
+function zero(time){
+    return time < 10 ? `0${time}` : time
+}
+
+setInterval(timerforNewYear, 1000)
